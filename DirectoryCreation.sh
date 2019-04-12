bf9582c3891f674855313dbad0f884f1cd834785
Himank1234@ <himank2602gupta@gmail.com>
2019-04-12 11:58:12 +0530

    directy creation

---
+++ DirectoryCreation.sh
@@ -0,0 +1,9 @@
+mkdir Assets && mkdir Assets/Global-Shard && mkdir Assets/Images
+mkdir Components 
+mkdir Configs
+mkdir Constants  
+mkdir Lib && mkdir Lib/business/Apis/private && mkdir Lib/business/Apis/public  && mkdir Lib/business/Apis/public/Business && mkdir Lib/business/Constants  && mkdir Lib/business/Utils
+mkdir Routes
+mkdir Scenes
+mkdir Util
+mkdir Wrappers && mkdir Wrappers/Modal-Wrapper && mkdir Wrappers/Modal-Wrapper/Templates &&  mkdir Wrappers/Modal-Wrapper/Templates/Modal-Footer && mkdir Wrappers/Modal-Wrapper/Templates/Modal-Header
